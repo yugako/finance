5f19f969f2c1059ea74bc66082818582caff6517
George Hasiuk <yurko25@outlook.com>
2020-02-08 07:54:30 +0200

    Fix path

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const config = require('config');
 const mongoose = require('mongoose');
-
+const path = require('path');
 const app = express();
 const PORT = config.get('port')|| 5000;
 
