6e796da2015b1303ba7b765e24e19823a3bf0226
George Hasiuk <yurko25@outlook.com>
2020-02-09 02:29:12 +0200

    Login/register

--- client/src/app/login/index.js
+++ client/src/app/login/index.js
@@ -1,8 +1,47 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
 
+import Pig from '../../assets/images/pig.svg';
 const Login = () => {
     return (
-        <h1>Login</h1>
+        <section className='login'>
+            <div className="container-fluid">
+                <div className="row align-items-center">
+                    <div className="col-12 col-lg-6 h-100">
+                        <img src={Pig} alt=""/>
+                    </div>
+                    <div className="col-12 col-lg-6 h-100">
+                        <div className="login-title title">
+                                Sign in
+                        </div>
+                        <form>
+                            <div class="form-group">
+                                <input required type="email"/>
+                                <label for="input" class="control-label">Email address</label>
+                                <i class="bar"></i>
+                            </div>
+                            <div class="form-group">
+                                <input required type="password"/>
+                                <label for="input" class="control-label">Password</label>
+                                <i class="bar"></i>
+                            </div>
+                            <input type="submit" className='submit' value="Sign Up"/>
+                        </form>
+                        <div className="login-cta d-flex justify-content-between">
+                            <div className="go-auth">
+                                Don’t have an account?
+                                <NavLink to='/register'>Sign Up</NavLink>
+                            </div>
+                            <div className="go-home">
+                                <NavLink to='/'>Back To Home</NavLink>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+           
+        </section>
     );
 }
  
