1c6a55f3871c1d492b18e650f511451f46a4374e
G.G <yurko25@outlook.com>
2020-02-24 21:38:32 +0200

    Chart on account single

--- client/src/app/dashboard/accounts/singleAccount/index.js
+++ client/src/app/dashboard/accounts/singleAccount/index.js
@@ -1,4 +1,8 @@
 import React, { useState, useCallback, useContext, useEffect } from 'react';
+import {
+  AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer
+} from 'recharts';
+
 
 import { useParams } from 'react-router-dom';
 import { useHttp } from '../../../../hooks/http.hook';
@@ -10,23 +14,44 @@ const SingleAccount = () => {
     const {request, loading} = useHttp();
     const {token} = useContext(AuthContext);
     const [account , setAccount] = useState(null);
+    const [activity , setActivity] = useState(null);
+
     const accountId = useParams().id;
 
-    const getAccount = useCallback(async () => {
+    const getData = useCallback(async () => {
         try {
             const accountData = await request(`/api/account/${accountId}`, 'GET', null, {
                 Authorization: `Bearer ${token}`
             });
 
+            const activityList = await request('/api/activity', 'GET', null, {
+                Authorization: `Bearer ${token}`
+            });
+
+            setActivity(activityList);
             setAccount(accountData);
         } catch (e) {
             
         }
     }, [token, accountId, request]);
 
+    const accountActivity = (accountName, accountBalance) => {
+        const current = !loading && activity && activity.filter(a =>  a.accountName === accountName);
+
+        const data = !loading && current && current.map(c => {
+            return {
+                sortDate: new Date(c.activityDate).getTime(),
+                displayDate: new Date(c.activityDate).toLocaleDateString(),
+                currentBalance: parseFloat(accountBalance) - c.activitySpendings
+            };
+        }).sort((a,b) => a.date - b.date).reverse();
+
+        return data;
+    }
+
     useEffect(() => {
-        getAccount();
-    }, [getAccount]);
+        getData();
+    }, [getData]);
 
     if (loading) {
         return <Loader />;
@@ -36,8 +61,23 @@ const SingleAccount = () => {
         <>
             {!loading && account && 
                 <section>
+                {console.log(accountActivity(account.acountName, account.balance))}
                     <TopBar title={`Account: ${account.acountName}`} />
-                     
+                     <ResponsiveContainer width="100%"
+                        height={400}>
+                    <AreaChart
+                        data={accountActivity(account.acountName, account.balance)}
+                        margin={{
+                            top: 10, right: 30, left: 0, bottom: 0,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="displayDate" />
+                        <YAxis dataKey="currentBalance"/>
+                        <Tooltip />
+                        <Area type="monotone" dataKey="currentBalance" stroke="#8884d8" fill="#8884d8" />
+                    </AreaChart>
+                  </ResponsiveContainer>
                 </section>
             }
         </>
