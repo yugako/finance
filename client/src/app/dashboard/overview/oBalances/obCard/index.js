2be795b4b4ba1e4b8c606bd10ce6a26da8cbe3a9
George Hasiuk <yurko25@outlook.com>
2020-03-07 02:18:12 +0200

    Fix data fetching

--- client/src/app/dashboard/overview/oBalances/obCard/index.js
+++ client/src/app/dashboard/overview/oBalances/obCard/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, } from 'recharts';
 
+
 import './index.scss';
 
 const BalancesCard = ({title, link, money, currency, datas}) => {
